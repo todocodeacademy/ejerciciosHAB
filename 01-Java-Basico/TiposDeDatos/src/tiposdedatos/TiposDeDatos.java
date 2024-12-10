a92d53322bf6b1055fcb3e56ea4da721268876bb
todocodeacademy <admin@todocodeacademy.com>
2024-12-09 21:48:28 -0300

    Agrego ejercicio de tipos de datos

---
+++ 01-Java-Basico/TiposDeDatos/src/tiposdedatos/TiposDeDatos.java
@@ -0,0 +1,20 @@
+package tiposdedatos;
+
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+      
+        //tipo de dato + nombre de la variable
+        int edad = 12;
+        String nombre = "Ibra";
+        char letra = 'a';
+        boolean bandera = false;
+        double estatura = 1.67;
+        
+        System.out.println("Hola " + nombre + " tu edad es: " + edad);
+        
+        
+        
+    }
+    
+}
